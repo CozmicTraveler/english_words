6571f1bbd02eb96855f96cf314e7b1054a942346
CozmicTraveler <kazumarice@hotmail.co.jp>
2024-01-04 23:22:52 +0900

    second commit

--- app/Services/EnglishService.php
+++ app/Services/EnglishService.php
@@ -7,18 +7,22 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class EnglishService{
+    //Return 20 words from word list.
     public function getWords()
     {
         return English::where('user_id',1)->orderBy('created_at','DESC')->take(20)->get();
     }
+
     public function myWords()
     {
+        //If not authenticated, return false.
         $user=Auth::user();
         if(is_null($user)){
             return false;
         }
         return English::where('created_by',$user['name'])->orderBy('created_at','DESC')->take(10)->get();
     }
+    //Return true or false about user-registered words.
     public function checkOwnWord(int $userId,int $wordId):bool
     {
         $word=English::where('id',$wordId)->first();
