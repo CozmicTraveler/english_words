d56e66134c7b2c6ec39f2d90378a97363a7e098e
CozmicTraveler <kazumarice@hotmail.co.jp>
2024-01-11 23:35:57 +0900

    9th commit

--- app/Models/English.php
+++ app/Models/English.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//Definition of English words.
 class English extends Model
 {
     use HasFactory;
@@ -13,7 +14,12 @@ class English extends Model
     *@var bool
     *
     **/
+    //Using timestamps explicitly(timestamps are activated by default).
     public $timestamps=true;
+    
+    //Designate table's name.
     protected $table='english';
+
+    //To activate created_at and updataed_at, add the columns to $fillable or $guarded.
     protected $fillable = ['user_id','word','meaning1','meaning2','memo','created_by'];
 }
